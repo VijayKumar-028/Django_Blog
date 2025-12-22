58eace853037fc0cc2000ace78cff6ae73e8c8b6
VijayKumar-028 <valivetivijaykumar05@gmail.com>
2025-12-22 22:26:29 +0530

    Added Comments

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,9 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),#TO get the detail view of the single post if we click on the post
+    path('post/new/', PostCreateView.as_view(), name='post-create'),#To create the new post
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),# To update the exisitng post
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'), # To delete the existing post
     path('about/',views.about,name='blog-about')
 ]
